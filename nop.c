6e0650fe504561810a9a4940e853cb905ba5c7b0
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-30 00:37:43 +0100

    new modif

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/*
+ * nop - doesn't do anything.
+ * @stack: stack pointer
+ * @line_number: number of line
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
